866f3adbcc7bfdd8ab75091fe3b8b79fe7bd0d73
recepkuskaya <37511691+recepkuskaya@users.noreply.github.com>
2025-03-23 17:22:20 +0300

    program dosyası değiştirildi

--- Program.cs
+++ Program.cs
@@ -3,4 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/btk", () => "Btk git deneme");
+
 app.Run();
--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1a8bd0684e35e1e393bcb28027b812c6d05cc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
