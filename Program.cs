b6d31e17cddbdfd3154392f89856f5867d605c2a
recepkuskaya <37511691+recepkuskaya@users.noreply.github.com>
2025-03-23 17:37:48 +0300

    mvc template oluşturuldu

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
 
-app.MapGet("/git", () => "git kurulumu");
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
