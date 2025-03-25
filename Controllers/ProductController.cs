d4b0feb89639377ca0353cd7a27f16668fc14146
recepkuskaya <recepgs3839@gmail.com>
2025-03-25 16:24:38 +0300

    product controller ve view oluşturuldu, db den veri çekildi

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+    }
+}
