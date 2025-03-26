72cc0fe0c4f412108b0351e4ca9035cde28bf8ac
recepkuskaya <recepgs3839@gmail.com>
2025-03-26 16:02:36 +0300

    Layout tanımı, viewStart, viewImport tanımları yapıldı.

--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b0feb89639377ca0353cd7a27f16668fc14146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770b9a8f1dc35af107866fed9c3163b1b00d33f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
